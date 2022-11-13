cd3a22a850c23bba6a9a234bc64f9f3a758e24a1
“sari-shiraki” <sari10702@gmail.com>
2022-11-13 15:08:54 +0900

    cors設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'foods/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'restaurants#index'
   resources :restaurants, only: [:index, :show] do
     resources :foods, only: :index
   end
